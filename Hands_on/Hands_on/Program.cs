be39039db7a091e8ab4ee52c6c8552925e44f8d3
Hareesh Anand <hareesha@infinite.com>
2024-11-10 23:27:22 +0530

    Assignment 4

---
+++ Hands_on/Hands_on/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hands_on
+{
+    class EmptyException : Exception
+    {
+        public EmptyException(string message) : base(message) { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter your name: ");
+                string name = Console.ReadLine();
+
+                if (name == "")
+                {
+                    throw new EmptyException("Name cannot be empty. Please enter a valid name.");
+                }
+
+                string upperName = name.ToUpper();
+                Console.WriteLine("Your name in uppercase is: " + upperName);
+            }
+            catch (EmptyException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            Console.Read();
+        }
+    }
+}
