134daf47ec67db0ac34df095aad6358ea39f2f1b
Hareesh Anand <hareesha@infinite.com>
2024-11-17 23:28:25 +0530

    Assignment 6

---
+++ HandsOn/HandsOn/NullOperations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOn
+{
+    class NullOperations
+    {
+        static void Main(string[] Args) 
+        {
+            Nullable<int> n = null;
+            int? x = null;
+
+            if(n.HasValue)
+                Console.WriteLine(n.Value);
+            else
+                Console.WriteLine("Null");
+            Console.Read();
+        }
+    }
+}
