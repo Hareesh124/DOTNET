97cd775d63e97edc616879832ef7b89cc60c982c
Hareesh Anand <hareesha@infinite.com>
2024-11-20 16:32:07 +0530

    Assesment 3

--- Assesment/Assesment3/Assesment3/FileWrite.cs
+++ Assesment/Assesment3/Assesment3/FileWrite.cs
@@ -16,10 +16,9 @@ namespace Assesment3
             Console.Write("EnTer the text you want to append: ");
             string inputText = Console.ReadLine();
 
-            using (StreamWriter writer = new StreamWriter(fileName, true))
-            {
-                writer.WriteLine(inputText);
-            }
+            StreamWriter writer = new StreamWriter(fileName, true);
+            writer.WriteLine(inputText);
+           
 
             Console.WriteLine($"The Text - {inputText} - has been appended to the file '{fileName}'.");
             Console.Read();
