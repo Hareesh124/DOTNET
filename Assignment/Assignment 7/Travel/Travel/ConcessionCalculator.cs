076d903765e6b8dc6f960b30634a7e28365e5f79
Hareesh Anand <hareesha@infinite.com>
2024-11-25 13:00:48 +0530

    Assignment 6

---
+++ Assignment/Assignment 7/Travel/Travel/ConcessionCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TravelConcessionLibrary
+{
+    public class ConcessionCalculator
+    {
+        int TotalFare = 500;
+
+        public void CalculateConcession(string name, int age)
+        {
+            if (age <= 5)
+            {
+                Console.WriteLine($"{name}, Little Champs - Free Ticket");
+            }
+            else if (age > 60)
+            {
+                double concessionFare = TotalFare * 0.7;
+                Console.WriteLine($"{name}, Senior Citizen - Concession Fare: {concessionFare}");
+            }
+            else
+            {
+                Console.WriteLine($"{name}, Ticket Booked - Fare: {TotalFare}");
+            }
+        }
+    }
+}
