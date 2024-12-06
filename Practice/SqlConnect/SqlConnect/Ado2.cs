32a8d8db44103ab0c25419319bd90e80b0848555
Hareesh Anand <hareesha@infinite.com>
2024-12-06 12:00:50 +0530

    Assignment 4 and 5

---
+++ Practice/SqlConnect/SqlConnect/Ado2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Ado2
+{
+    class Region
+    {
+        public int RegionID { get; set; }
+        public string RegionDescription { get; set; }
+
+        DataAccess da = new DataAccess();
+        internal void InsertRegion()
+        {
+            Console.WriteLine("Enter The Region ID :");
+            RegionID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Region Description :");
+            RegionDescription = Console.ReadLine();
+            da.InsertRegion(RegionID, RegionDescription);
+        }
+
+        internal void SelectRegions()
+        {
+            da.SelectData();
+        }
+    }
+
+    class DataAccess
+    {
+        static SqlConnection conn = null;
+        static SqlCommand cmd = null;
+
+        public SqlConnection getConnection()
+        {
+            string connectstr = "Data Source=ICS-LT-GSRQ4D3;Initial Catalog=northwin;" +
+                "Integrated Security=True;";
+            conn = new SqlConnection(connectstr);
+            conn.Open();
+            return conn;
+        }
+
+        public void SelectData()
+        {
+            try
+            {
+                conn = getConnection();
+                cmd = new SqlCommand("Select * from Region", conn);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Console.WriteLine(dr["RegionID"] + " " + dr["RegionDescription"]);
+                }
+            }
+            catch (SqlException se)
+            {
+                Console.WriteLine(se.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void InsertRegion(int rid, string rdesc)
+        {
+            try
+            {
+                conn = getConnection();
+                cmd = new SqlCommand("insert into region values(@rgid,@rgdesc)", conn);
+                cmd.Parameters.AddWithValue("@rgid", rid);
+                cmd.Parameters.AddWithValue("@rgdesc", rdesc);
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException se)
+            {
+                Console.WriteLine(se.Message);
+            }
+        }
+    }
+    class Ado2
+    {
+        public static void Main()
+        {
+            Region region = new Region();
+            region.SelectRegions();
+            Console.WriteLine();
+            region.InsertRegion();
+            Console.WriteLine();
+            region.SelectRegions();
+            Console.Read();
+        }
+    }
+}
---
+++ Practice/SqlConnect/SqlConnect/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SqlConnect
+{
+    class Program
+    {
+        public static SqlConnection con;
+        public static SqlCommand cmd;
+        public static IDataReader dr;
+        static void Main()
+        {
+            //InsertData();
+            //DeleteData();
+            //SelectData();
+            UpdateData();
+            Console.Read();
+        }
+
+        static SqlConnection getConnection()
+        {
+            con = new SqlConnection("Data Source=ICS-LT-GSRQ4D3;Initial Catalog=company;" +
+                "Integrated Security=True;");
+            con.Open();
+
+            return con;
+        }
+        static void SelectData()
+        {
+            con = getConnection();
+            cmd = new SqlCommand("select * from tblemployee");
+            cmd.Connection = con;
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.WriteLine(dr[0] + " " + dr[1] + " " + dr[2] + " " + dr[3] + " " + dr[4]);
+
+                //Console.WriteLine("Employee ID :" + dr[0]);
+                //Console.WriteLine("Employee Name :" + dr[1]);
+                //Console.WriteLine("Employee Salary :" + dr[3]);
+            }
+
+        }
+
+        static void InsertData()
+        {
+            con = getConnection();
+            Console.WriteLine("Enter Empid, EmpName, Gender,Salary,Dept,Phone :");
+            int eid = Convert.ToInt32(Console.ReadLine());
+            string ename = Console.ReadLine();
+            string egen = Console.ReadLine();
+            float esal = Convert.ToSingle(Console.ReadLine());
+            int edept = Convert.ToInt32(Console.ReadLine());
+            string ephone = Console.ReadLine();
+            //cmd=new SqlCommand("Insert into tblemployee values(150,'Rama','Male',15000,5,0000007)",con);
+
+            cmd = new SqlCommand("Insert into tblemployee values(@ecode,@empname,@egender@esalary,@edid,@eph)", con);
+            //all the above variables are now appended to the parameters collection of the command object
+            cmd.Parameters.AddWithValue("@ecode", eid);
+            cmd.Parameters.AddWithValue("@empname", ename);
+            cmd.Parameters.AddWithValue("@egender", egen);
+            cmd.Parameters.AddWithValue("@esalary", esal);
+            cmd.Parameters.AddWithValue("@edid", edept);
+            cmd.Parameters.AddWithValue("@eph", ephone);
+
+            int res = cmd.ExecuteNonQuery();
+            if (res > 0)
+                Console.WriteLine("Inserted Successfully");
+            else
+                Console.WriteLine("Could not Insert..");
+        }
+
+        static void DeleteData()
+        {
+            con = getConnection();
+            Console.WriteLine("Enter Empid to delete :");
+            string eid = Console.ReadLine();
+            SqlCommand cmd1 = new SqlCommand("select * from tblemployee where empid=@eid");
+            cmd1.Connection = con;
+            cmd1.Parameters.AddWithValue("@eid", eid);
+            SqlDataReader dr1 = cmd1.ExecuteReader();
+
+            while (dr1.Read())
+            {
+                for (int i = 0; i < dr1.FieldCount; i++)
+                {
+                    Console.WriteLine(dr1[i]);
+                }
+            }
+            con.Close();
+            Console.WriteLine();
+            Console.WriteLine("Are You sure to delete this Employee ( Y/N )? :");
+            string answer = Console.ReadLine();
+            if (answer == "Y" || answer == "y")
+            {
+                cmd = new SqlCommand("delete from tblemployee where empid=@eid", con);
+                cmd.Parameters.AddWithValue("@eid", eid);
+                con.Open();
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Record Deleted Successfully..");
+            }
+        }
+
+        static void UpdateData()
+        {
+            con = getConnection();
+            Console.WriteLine("Enter Empid to update : ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            float sal;
+            Console.WriteLine("Enter the updated salary : ");
+            sal = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Data Updated");
+            SqlCommand cmd3 = new SqlCommand($"update Emp set sal = {sal} where empno = {id}", con);
+            cmd3.Connection = con;
+            cmd3.Parameters.AddWithValue("@empno", id);
+            SqlDataReader dr2 = cmd3.ExecuteReader();
+
+            while (dr2.Read())
+            {
+                for(int i = 0;i <dr.FieldCount; i++)
+                {
+                    Console.WriteLine(dr2[i]);
+                }
+            }
+            con.Close();
+        }
+    }
+}
